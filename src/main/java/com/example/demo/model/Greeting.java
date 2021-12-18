c59bcd148f7a93b60ab6810f4992b7afaf965381
unknown <gayoyawalkar@gmail.com>
2021-12-18 19:10:45 +0530

    Added:Greeting Controller Http Methods Uc1

---
+++ src/main/java/com/example/demo/model/Greeting.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+public class Greeting {
+	private long id;
+	private String message;
+	public Greeting(long id,String message) {
+		this.id=id;
+		this.message=message;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getMessage() {
+		return message;
+	}
+}
