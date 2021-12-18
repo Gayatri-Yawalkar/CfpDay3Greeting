1bfb0760e87c587d82b28fed09d4cded0a1cd130
unknown <gayoyawalkar@gmail.com>
2021-12-18 19:12:40 +0530

    Added:Greeting Controller with service Layer Uc2

--- src/main/java/com/example/demo/model/Greeting.java
+++ src/main/java/com/example/demo/model/Greeting.java
@@ -1,5 +1,11 @@
 package com.example.demo.model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="GREETINGS")
 public class Greeting {
+	@Id
 	private long id;
 	private String message;
 	public Greeting(long id,String message) {
--- src/test/java/com/example/demo/Day3GreetingApplicationTests.java
+++ src/test/java/com/example/demo/Day3GreetingApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Day2LoggerApplicationTests {
+class Day3GreetingApplicationTests {
 
 	@Test
 	void contextLoads() {
